b589070787c735aa71a39289e30955eaecd28258
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2021-08-01 11:47:45 +0200

    Add deployment playbooks

--- global_config.tf
+++ global_config.tf
@@ -12,25 +12,27 @@ variable "os_image_catalog" {
     default = {
         "arch" = {
             disk = "arch-2021.06.01-x86_64.qcow2",
-            provision_playbook = "rename.yml"
+            provision_playbook = "linux_deploy.yml"
             ansible_user = "user"
         },
         "debian10" = {
             disk = "debian10-10.10.0-amd64.qcow2",
-            provision_playbook = "rename.yml"
+            provision_playbook = "linux_deploy.yml"
             ansible_user = "user"
         },
         "windows10" = {
             disk = "windows10-20H2_v2-x86_64.qcow2",
-            provision_playbook = "windows_rename.yml"
+            provision_playbook = "windows_deploy.yml"
             ansible_user = "administrator"
         }
     }
 }
 
-# Set by the TF_VAR_ansible_playbooks environment variable
+# Set by the TF_VAR_ansible_playbooks environment variable if you want to manage
+# this centrally and not use the default ./ansible/ path
 variable "ansible_playbooks" {
-    description = "Path to the ansible playbook directory. Uses the TF_VAR_ansible_playbooks environment variable"
+description = "Path to the ansible playbook directory. Uses the TF_VAR_ansible_playbooks environment variable if set. Defaults to ./ansible/"
+    default = "./ansible"
 }
 
 variable "network_tld" {
