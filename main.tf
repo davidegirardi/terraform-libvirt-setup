9266600dead50def869e929d733b2c8d2ad73822
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2021-08-03 11:24:10 +0200

    Comment

--- main.tf
+++ main.tf
@@ -10,6 +10,22 @@ provider "libvirt" {
     uri = "qemu:///system"
 }
 
+##############################################
+#                                            #
+#    Create storage pool for the project     #
+#                                            #
+##############################################
+resource "libvirt_pool" "project_pool" {
+    name = "${var.project_name}"
+    type = "dir"
+    path = "${var.pool_prefix}/${var.project_name}"
+}
+
+##################################################
+#                                                #
+#    Create dedicated network for the project    #
+#                                                #
+##################################################
 resource "libvirt_network" "project_network" {
     name = "${var.project_name}.${var.network_tld}"
     domain = "${var.project_name}.${var.network_tld}"
@@ -18,27 +34,28 @@ resource "libvirt_network" "project_network" {
     dns {
         local_only = true
     }
-   provisioner "local-exec" {
-        command = <<-EOC
-            echo "server=/${self.domain}/${var.dnsmasq_listen}" | sudo tee -a /etc/NetworkManager/dnsmasq.d/libvirtd_dnsmasq.conf
-            sudo systemctl reload NetworkManager.service
-        EOC
-   }
-   provisioner "local-exec" {
-        when = destroy
-        command = <<-EOC
-            sudo sed -i '\/server=\/${self.domain}\/.*$/d' /etc/NetworkManager/dnsmasq.d/libvirtd_dnsmasq.conf
-            sudo systemctl reload NetworkManager.service
-        EOC
-   }
+    # Configure name resolution in Network Manager, REQUIRES SUDO!
+    provisioner "local-exec" {
+         command = <<-EOC
+             echo "server=/${self.domain}/${var.dnsmasq_listen}" | sudo tee -a /etc/NetworkManager/dnsmasq.d/libvirtd_dnsmasq.conf
+             sudo systemctl reload NetworkManager.service
+         EOC
+    }
+    # Remove name resolution from Network Manager on destroy, REQUIRES SUDO!
+    provisioner "local-exec" {
+         when = destroy
+         command = <<-EOC
+             sudo sed -i '\/server=\/${self.domain}\/.*$/d' /etc/NetworkManager/dnsmasq.d/libvirtd_dnsmasq.conf
+             sudo systemctl reload NetworkManager.service
+         EOC
+    }
 }
 
-resource "libvirt_pool" "project_pool" {
-    name = "${var.project_name}"
-    type = "dir"
-    path = "${var.pool_prefix}/${var.project_name}"
-}
-
+#############################################################
+#                                                           #
+#    Create base volumes shared by the Operating Systems    #
+#                                                           #
+#############################################################
 module "auto_base_volume" {
     source = "./modules/auto_base_volume"
     # From global_config.tf
@@ -51,14 +68,25 @@ module "auto_base_volume" {
     project_pool_name = libvirt_pool.project_pool.name
 }
 
+####################################
+#                                  #
+#    Create volumes for the VMs    #
+#                                  #
+####################################
 resource "libvirt_volume" "cow_volume" {
     for_each = var.project_machines
     name   = "${each.key}.${libvirt_network.project_network.domain}.qcow2"
+    # Use the base volume created above
     base_volume_id = module.auto_base_volume.volumes["${each.value.distro}"].id
     base_volume_pool = libvirt_pool.project_pool.name
     pool = libvirt_pool.project_pool.name
 }
 
+#############################
+#                           #
+#    Create the machines    #
+#                           #
+#############################
 resource "libvirt_domain" "domain" {
     for_each = var.project_machines
     name = "${each.key}.${libvirt_network.project_network.domain}"
@@ -82,17 +110,20 @@ resource "libvirt_domain" "domain" {
     xml {
         xslt = file("add_spice.xsl")
     }
-   dynamic "filesystem" {
-       for_each = each.value.filesystems
-       content {
-           source = filesystem.value["source"]
-           target = filesystem.key
-           readonly = filesystem.value["readonly"]
-       }
-   }
-   provisioner "local-exec" {
-        command = <<-EOC
-            ansible-playbook -u ${var.os_image_catalog["${each.value.distro}"].ansible_user} -i %{ if replace(self.network_interface[0].addresses[0], ":", "") == self.network_interface[0].addresses[0] }${self.network_interface[0].addresses[0]}%{ else }${self.network_interface[0].addresses[1]}%{ endif}, -e newhostname=${self.name} ${var.ansible_playbooks}/${var.os_image_catalog[each.value.distro].provision_playbook}
-        EOC
-   }
+    # Shared folders
+    dynamic "filesystem" {
+        for_each = each.value.filesystems
+        content {
+            source = filesystem.value["source"]
+            target = filesystem.key
+            readonly = filesystem.value["readonly"]
+        }
+    }
+    # Deployment playbook: name the machine and do other stuff
+    # See: ansible/linux_deploy.yml and ansible/windows_deploy.yml
+    provisioner "local-exec" {
+         command = <<-EOC
+             ansible-playbook -u ${var.os_image_catalog["${each.value.distro}"].ansible_user} -i %{ if replace(self.network_interface[0].addresses[0], ":", "") == self.network_interface[0].addresses[0] }${self.network_interface[0].addresses[0]}%{ else }${self.network_interface[0].addresses[1]}%{ endif}, -e newhostname=${self.name} ${var.ansible_playbooks}/${var.os_image_catalog[each.value.distro].provision_playbook}
+         EOC
+    }
 }
