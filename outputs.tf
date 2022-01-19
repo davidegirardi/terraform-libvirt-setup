974fc098bf1c9dce2ee73f4e3b2aac4e0c5d0c1e
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2022-01-19 19:19:10 +0100

    Remove output message

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "message" {
-  description = "Inform about output files"
-  value       = "See inventory.json for ansible and ssh_config.conf for ssh"
-}
-
 resource "local_file" "ansible_inventory" {
   filename = "inventory.json"
   content = templatefile("file_templates/ansible_inventory.tmpl", {
@@ -10,6 +5,7 @@ resource "local_file" "ansible_inventory" {
   })
   file_permission = "0644"
 }
+
 resource "local_file" "ssh_config" {
   filename = "ssh_config.conf"
   content = templatefile("file_templates/ssh_config.tmpl", {
