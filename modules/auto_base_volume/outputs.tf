0cb46409e00d569dc105eb8ae4ea39fb54a2e8a7
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2022-01-19 19:10:36 +0100

    Format

--- modules/auto_base_volume/outputs.tf
+++ modules/auto_base_volume/outputs.tf
@@ -1,4 +1,4 @@
 output "volumes" {
-    description = "Base volume for consolidation of the distros (OSs) running in the project"
-    value = libvirt_volume.base_volume
+  description = "Base volume for consolidation of the distros (OSs) running in the project"
+  value       = libvirt_volume.base_volume
 }
--- modules/auto_base_volume/variables.tf
+++ modules/auto_base_volume/variables.tf
@@ -1,31 +1,31 @@
 variable "project_name" {
-    description = "Commont prefix used by all the VMs"
-    type = string
+  description = "Commont prefix used by all the VMs"
+  type        = string
 }
 
 variable "project_pool_name" {
-    description = "Name of the pool to use to create the disks"
-    type = string
+  description = "Name of the pool to use to create the disks"
+  type        = string
 }
 
 variable "project_machines" {
-    description = "Map of project names to configuration."
-    type = map
+  description = "Map of project names to configuration."
+  type        = map(any)
 }
 
 # Set by the TF_VAR_templates_path environment variable
 variable "templates_path" {
-    description = "Path to the template disks built by packer, uses the TF_VAR_templates_path environment variable"
+  description = "Path to the template disks built by packer, uses the TF_VAR_templates_path environment variable"
 }
 
 # Set by the TF_VAR_ansible_playbooks environment variable
 variable "ansible_playbooks" {
-    description = "Path to the ansible playbook directory. Uses the TF_VAR_ansible_playbooks environment variable"
+  description = "Path to the ansible playbook directory. Uses the TF_VAR_ansible_playbooks environment variable"
 }
 
 
 variable "os_image_catalog" {
-    description = "Map operating systems to base disk images"
-    type = map
+  description = "Map operating systems to base disk images"
+  type        = map(any)
 }
 
