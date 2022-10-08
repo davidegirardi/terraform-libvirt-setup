042e2d6926ed75dccf79fb69f9405456d76959c3
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2022-10-08 08:49:18 +0200

    Add autodownload

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ In `/etc/libvirt/qemu.conf`
 user = "yourusername"
 groups = "yourgroup"
 ```
+
+Set the following environment variables to set:
+- The directory containing all of the pools via `TF_VAR_pool_prefix`
+- The local path on the filesystem containing the base images via`TF_VAR_templates_path`
+
+## Caching Cloud Images
+Head over to `autodownload/` and run `terraform apply` to download today's copy of the cloud images in `TF_VAR_templates_path`.
+
---
+++ autodownload/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+resource "null_resource" "get_images" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  for_each = {
+    for imagename, imagedata in var.os_image_catalog : imagename => imagedata
+    if substr(imagedata.disk, 0, 8) == "https://"
+  }
+  provisioner "local-exec" {
+    command = <<-EOC
+      wget --quiet ${each.value.disk} -O ${var.templates_path}/${basename(each.value.disk)}
+   EOC
+  }
+}
+
